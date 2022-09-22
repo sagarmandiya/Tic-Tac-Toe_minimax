0be2b6a856ab60a6f5b54313e1c60e84cc2086b1
Aaron B <aaronbo@bu.edu>
2022-09-21 20:36:56 -0400

    Create ttt.py

---
+++ ttt.py
@@ -0,0 +1,63 @@
+Class TicTacToe
+
+    Moves - number of moves so far
+    Player1 - goes 1st
+    Player2 - goes 2nd
+    Board - string vector of length 9 ('-','X','O')
+    [Move is a vector of position (0-8) and symbol ('X','O')]
+    
+    Initializer
+        Player1 picks X or O? (or is this in play game)
+
+    def __init__(self):
+        self.moves = 0
+        self.board = ['-'] * 9
+        self.play()
+
+
+    Draw board function
+
+
+    Accept Input
+    - validate move
+    -> Output
+
+    Find best move
+    - Uses max and min functions recursively
+
+    Max function
+
+    Min function
+
+    Play again prompt function
+
+    def check_game_over(self):
+
+        # Winning combinations:
+        # Horizontal: (0,1,2), (3,4,5), (6,7,8)
+        # Vertical: (0,3,6), (1,4,7), (2,5,8)
+        # Diagonal: (0,4,8), (2,4,6)
+        Output is 'X', 'O', or 'Cats'
+        
+
+    def play(self):
+        while True:
+            
+            Draw board function
+            if self.check_game_over() != "":
+                Output Result function(self.check_game_over())
+                Play again prompt function
+                break
+            else:
+                self.moves++
+
+            if (moves % 2) == 0:
+                Player2's turn
+            else:
+                Player1's turn
+            
+            
+
+
+Main
+- calls game class to play
