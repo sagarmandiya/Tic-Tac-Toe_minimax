23ce8e8a62d63a81c665005aa5753ad741737269
Aaron B <aaronbo@bu.edu>
2022-09-21 20:44:13 -0400

    Update ttt.py

--- ttt.py
+++ ttt.py
@@ -37,6 +37,15 @@ Class TicTacToe
         # Horizontal: (0,1,2), (3,4,5), (6,7,8)
         # Vertical: (0,3,6), (1,4,7), (2,5,8)
         # Diagonal: (0,4,8), (2,4,6)
+
+        # Horizontal Wins
+        if self.board[0] == self.board[1] == self.board[2]:
+            return self.board[0]
+        elif self.board[3] == self.board[4] == self.board[5]:
+            return self.board[3]
+        elif self.board[6] == self.board[7] == self.board[8]:
+            return self.board[6]
+        
         Output is 'X', 'O', or 'Cats'
         
 
